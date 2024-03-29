51998d6aa0dd65fce8b0ea2e4de9e15ffa6b3a79
Bruce Lin <gaicilin@gmail.com>
2024-03-28 21:55:59 -0400

    refactored into function and completed basic tests

--- fuzzylogic.py
+++ fuzzylogic.py
@@ -1,54 +1,82 @@
+from typing import Union
 from matplotlib import pyplot as plt
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-# Create the universe of discourse
-wind = ctrl.Antecedent(np.arange(10, 50, 1), "wind")
-snow = ctrl.Antecedent(np.arange(0, 201, 1), "snow")
-risk = ctrl.Consequent(np.arange(0, 11, 1), "risk")  # 0 means low, 10 means high
 
-# Generate fuzzy membership functions
-snow["light"] = fuzz.trimf(snow.universe, [0, 0, 76.2])  # up to 3 inches
-snow["moderate"] = fuzz.trimf(
-    snow.universe, [50.8, 101.6, 152.4]
-)  # Starts at 2 inches, ends at 6 inches
-snow["heavy"] = fuzz.trimf(snow.universe, [101.6, 152.4, 200])  # beyond 6 inches
-snow["very heavy"] = fuzz.trimf(snow.universe, [152.4, 176.2, 200])
+def get_fuzzy_output(
+    wind_speed: Union[int, float],
+    snow_precipitation: Union[int, float],
+    show_mf: bool = False,
+) -> float:
+    if wind_speed is None or snow_precipitation is None:
+        raise ValueError("Wind speed and snow precipitation cannot be None.")
 
-wind["gentle"] = fuzz.trimf(wind.universe, [0, 0, 24])
-wind["moderate"] = fuzz.trimf(wind.universe, [19, 31, 44])
-wind["strong"] = fuzz.trimf(wind.universe, [39, 44, 49])
+    # Convert inputs to float if they are int
+    wind_speed = float(wind_speed)
+    snow_precipitation = float(snow_precipitation)
 
-risk["low"] = fuzz.trimf(risk.universe, [0, 0, 4])
-risk["medium"] = fuzz.trimf(risk.universe, [3, 5, 7])
-risk["high"] = fuzz.trimf(risk.universe, [6, 10, 10])
+    if wind_speed <= 0 or snow_precipitation <= 0:
+        raise ValueError(
+            "Wind speed and snow precipitation cannot be negative or zero."
+        )
 
-# Define fuzzy rules
-rules = []
+    if wind_speed >= 49 or snow_precipitation >= 200:
+        raise ValueError(
+            "Wind speed and snow precipitation are out of the expected range."
+        )
 
-rules.append(ctrl.Rule(snow["light"] & wind["gentle"], risk["low"]))
-rules.append(ctrl.Rule(snow["moderate"] | wind["moderate"], risk["medium"]))
-rules.append(ctrl.Rule(snow["heavy"] & wind["moderate"], risk["high"]))
-rules.append(ctrl.Rule(snow["very heavy"] | wind["strong"], risk["high"]))
+    # Create the universe of discourse
+    wind = ctrl.Antecedent(np.arange(10, 50, 1), "wind")
+    snow = ctrl.Antecedent(np.arange(0, 201, 1), "snow")
+    risk = ctrl.Consequent(np.arange(0, 11, 1), "risk")  # 0 means low, 10 means high
 
-# Create control system
-risk_ctrl = ctrl.ControlSystem(rules)
+    # Generate fuzzy membership functions
+    snow["light"] = fuzz.trimf(snow.universe, [0, 0, 76.2])  # up to 3 inches
+    snow["moderate"] = fuzz.trimf(
+        snow.universe, [50.8, 101.6, 152.4]
+    )  # Starts at 2 inches, ends at 6 inches
+    snow["heavy"] = fuzz.trimf(snow.universe, [101.6, 152.4, 200])  # beyond 6 inches
+    snow["very heavy"] = fuzz.trimf(snow.universe, [152.4, 176.2, 200])
 
-# Create simulation
-driving = ctrl.ControlSystemSimulation(risk_ctrl)
+    wind["gentle"] = fuzz.trimf(wind.universe, [0, 0, 24])
+    wind["moderate"] = fuzz.trimf(wind.universe, [19, 31, 44])
+    wind["strong"] = fuzz.trimf(wind.universe, [39, 44, 49])
 
-# Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
-driving.input["wind"] = 18
-driving.input["snow"] = 60
+    risk["low"] = fuzz.trimf(risk.universe, [0, 0, 4])
+    risk["medium"] = fuzz.trimf(risk.universe, [3, 5, 7])
+    risk["high"] = fuzz.trimf(risk.universe, [6, 10, 10])
 
-# Crunch the numbers
-driving.compute()
+    # Define fuzzy rules
+    rules = []
 
-print(f"risk: {driving.output['risk']:.1f}")
+    rules.append(ctrl.Rule(snow["light"] & wind["gentle"], risk["low"]))
+    rules.append(ctrl.Rule(snow["moderate"] | wind["moderate"], risk["medium"]))
+    rules.append(ctrl.Rule(snow["heavy"] & wind["moderate"], risk["high"]))
+    rules.append(ctrl.Rule(snow["very heavy"] | wind["strong"], risk["high"]))
 
-wind.view()
-snow.view()
-risk.view()
-risk.view(sim=driving)
-plt.show()
+    # Create control system
+    risk_ctrl = ctrl.ControlSystem(rules)
+
+    # Create simulation
+    driving = ctrl.ControlSystemSimulation(risk_ctrl)
+
+    # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
+    driving.input["wind"] = wind_speed
+    driving.input["snow"] = snow_precipitation
+
+    # Crunch the numbers
+    driving.compute()
+
+    result = driving.output["risk"]
+    print(f"risk: {result:.1f}")
+
+    if show_mf:
+        wind.view()
+        snow.view()
+        risk.view()
+        risk.view(sim=driving)
+        plt.show()
+
+    return result
---
+++ test_fuzzylogic.py
@@ -0,0 +1,38 @@
+import pytest
+from fuzzylogic import get_fuzzy_output
+
+
+def test_normal_case():
+    # Normal case should not raise any exceptions and should return a float
+    result = get_fuzzy_output(12.55, 23.4)
+    assert isinstance(result, float), "The result should be a float"
+
+
+def test_inputs_are_int():
+    # Testing the integer inputs, should return a float
+    result = get_fuzzy_output(48, 199)
+    assert isinstance(result, float), "The result should be a float"
+
+
+def test_negative_inputs():
+    # Should raise ValueError for negative inputs
+    with pytest.raises(ValueError):
+        get_fuzzy_output(-1, -5)
+
+
+def test_zero_inputs():
+    # Should raise ValueError for negative inputs
+    with pytest.raises(ValueError):
+        get_fuzzy_output(0, 0)
+
+
+def test_inputs_are_none():
+    # Should raise ValueError when inputs are None
+    with pytest.raises(ValueError):
+        get_fuzzy_output(None, None)
+
+
+def test_inputs_out_of_range():
+    # Should raise ValueError for out of range inputs
+    with pytest.raises(ValueError):
+        get_fuzzy_output(50, 201)
