b52755b45fa9dc21ada0b689b438f839ebc917fe
Ky Huynh <hgky95@gmail.com>
2024-03-29 13:17:43 -0400

    integrate with Fuzzy Logic

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import tkinter as tk
 import tkinter.font as tkFont
-from tkinter import RAISED, NW
-
+import fuzzylogic as fuzzy
 from PIL import ImageTk, Image
 
 
 class App:
     def __init__(self, root):
-        # setting title
+        # setting title and config window size
         root.title("Car Driving Analyzer")
-        # setting window size
         width = 263
         height = 403
         screenwidth = root.winfo_screenwidth()
@@ -18,78 +16,100 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GButton_312 = tk.Button(root)
-        GButton_312["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times', size=10)
-        GButton_312["font"] = ft
-        GButton_312["fg"] = "#000000"
-        GButton_312["justify"] = "center"
-        GButton_312["text"] = "Analyze"
-        GButton_312.place(x=70, y=280, width=91, height=30)
-        GButton_312["command"] = self.GButton_312_command
-
-        GLineEdit_203 = tk.Entry(root)
-        GLineEdit_203["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times', size=10)
-        GLineEdit_203["font"] = ft
-        GLineEdit_203["fg"] = "#333333"
-        GLineEdit_203["justify"] = "center"
-        GLineEdit_203["text"] = "Entry"
-        GLineEdit_203.place(x=10, y=150, width=223, height=30)
-
-        GLabel_501 = tk.Label(root)
-        ft = tkFont.Font(family='Times', size=10)
-        GLabel_501["font"] = ft
-        GLabel_501["fg"] = "#333333"
-        GLabel_501["justify"] = "center"
-        GLabel_501["text"] = "Snow precipitation (mm)"
-        GLabel_501.place(x=10, y=110, width=144, height=30)
-
-        GLabel_27 = tk.Label(root)
-        ft = tkFont.Font(family='Times', size=10)
-        GLabel_27["font"] = ft
-        GLabel_27["fg"] = "#333333"
-        GLabel_27["justify"] = "center"
-        GLabel_27["text"] = "Wind speed (km/h)"
-        GLabel_27.place(x=10, y=190, width=114, height=30)
-
-        GLabel_734 = tk.Label(root)
-        ft = tkFont.Font(family='Times', size=10)
-        GLabel_734["font"] = ft
-        GLabel_734["fg"] = "#333333"
-        GLabel_734["justify"] = "center"
-        GLabel_734["text"] = "Car driving risk:"
-        GLabel_734.place(x=10, y=330, width=109, height=30)
-
-        GLineEdit_826 = tk.Entry(root)
-        GLineEdit_826["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times', size=10)
-        GLineEdit_826["font"] = ft
-        GLineEdit_826["fg"] = "#333333"
-        GLineEdit_826["justify"] = "center"
-        GLineEdit_826["text"] = "Entry2"
-        GLineEdit_826.place(x=10, y=230, width=221, height=30)
-
-        GMessage_480 = tk.Message(root)
-        ft = tkFont.Font(family='Times', size=10)
-        GMessage_480["font"] = ft
-        GMessage_480["fg"] = "#333333"
-        GMessage_480["justify"] = "center"
-        GMessage_480["text"] = "Message"
-        GMessage_480.place(x=120, y=330, width=80, height=25)
-
-        # Create an object of tkinter ImageTk
+        # Create image
         original_image = Image.open("car-weather.png")
         img_resize = original_image.resize((100, 100))
         img = ImageTk.PhotoImage(img_resize)
         # Create a Label Widget to display the text or Image
-        label = tk.Label(root, image = img)
+        label = tk.Label(root, image=img)
         label.pack()
         label.place(x=80, y=10, width=100, height=100)
+
+        # Create Snow precipitation label
+        snow_prec_label = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=15)
+        snow_prec_label["font"] = ft
+        snow_prec_label["fg"] = "#333333"
+        snow_prec_label["justify"] = "center"
+        snow_prec_label["text"] = "Snow precipitation (mm)"
+        snow_prec_label.place(x=10, y=110, width=160, height=30)
+
+        # Create Snow Precipitation entry
+        self.snow_prec_entry = tk.Entry(root)
+        snow_prec_entry = self.snow_prec_entry
+        snow_prec_entry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=15)
+        snow_prec_entry["font"] = ft
+        snow_prec_entry["fg"] = "#333333"
+        snow_prec_entry["justify"] = "center"
+        snow_prec_entry["text"] = "Snow Entry"
+        snow_prec_entry.place(x=10, y=150, width=223, height=30)
+
+        # Create Wind Speed label
+        wind_speed_label = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=15)
+        wind_speed_label["font"] = ft
+        wind_speed_label["fg"] = "#333333"
+        wind_speed_label["justify"] = "center"
+        wind_speed_label["text"] = "Wind speed (km/h)"
+        wind_speed_label.place(x=10, y=190, width=130, height=30)
+
+        # Create Wind Speed entry
+        self.wind_speed_entry = tk.Entry(root)
+        wind_speed_entry = self.wind_speed_entry
+        wind_speed_entry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=15)
+        wind_speed_entry["font"] = ft
+        wind_speed_entry["fg"] = "#333333"
+        wind_speed_entry["justify"] = "center"
+        wind_speed_entry["text"] = "Entry2"
+        wind_speed_entry.place(x=10, y=230, width=221, height=30)
+
+        # Create Analyze button
+        analyzer_btn = tk.Button(root)
+        analyzer_btn["bg"] = "#efefef"
+        ft = tkFont.Font(family='Times', size=15)
+        analyzer_btn["font"] = ft
+        analyzer_btn["fg"] = "#000000"
+        analyzer_btn["justify"] = "center"
+        analyzer_btn["text"] = "Analyze"
+        analyzer_btn.place(x=70, y=280, width=91, height=30)
+        analyzer_btn["command"] = self.analyze_weather
+
+        # Create Driving Risk Label
+        driving_risk_label = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=15)
+        driving_risk_label["font"] = ft
+        driving_risk_label["fg"] = "#333333"
+        driving_risk_label["justify"] = "center"
+        driving_risk_label["text"] = "Car driving risk:"
+        driving_risk_label.place(x=10, y=330, width=109, height=30)
+
+        # Create Driving Risk value
+        self.risk_level_msg = tk.Message(root)
+        risk_level_msg = self.risk_level_msg
+        ft = tkFont.Font(family='Times', size=15)
+        risk_level_msg["font"] = ft
+        risk_level_msg["fg"] = "#333333"
+        risk_level_msg["justify"] = "left"
+        risk_level_msg["text"] = ""
+        #risk_level_msg.place(x=120, y=333, width=80, height=25)
+        risk_level_msg.place(x=120, y=333, width=80, height=55)
         root.mainloop()
 
-    def GButton_312_command(self):
-        print("command")
+
+    def analyze_weather(self):
+        snow_precipitation = int(self.snow_prec_entry.get())
+        wind_speed = int(self.wind_speed_entry.get())
+
+        # Print the values
+        print("Snow precipitation:", snow_precipitation)
+        print("Wind speed:", wind_speed)
+
+        risk = fuzzy.get_fuzzy_output(wind_speed, snow_precipitation)
+        print(risk)
+        self.risk_level_msg.config(text=str(risk))
+
 
 
 if __name__ == "__main__":
