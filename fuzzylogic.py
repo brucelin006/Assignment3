dd1ef2bcb173d07995cdb842b74e9867c9f7b76b
Bruce Lin <gaicilin@gmail.com>
2024-03-29 14:12:53 -0400

    added few more tests

--- fuzzylogic.py
+++ fuzzylogic.py
@@ -33,16 +33,16 @@ def get_fuzzy_output(
     risk = ctrl.Consequent(np.arange(0, 11, 1), "risk")  # 0 means low, 10 means high
 
     # Generate fuzzy membership functions
+    wind["gentle"] = fuzz.trimf(wind.universe, [0, 0, 20])
+    wind["moderate"] = fuzz.trimf(wind.universe, [19, 31, 41])
+    wind["strong"] = fuzz.trimf(wind.universe, [39, 44, 49])
+
     snow["light"] = fuzz.trimf(snow.universe, [0, 0, 76.2])  # up to 3 inches
     snow["moderate"] = fuzz.trimf(
         snow.universe, [50.8, 101.6, 152.4]
     )  # Starts at 2 inches, ends at 6 inches
     snow["heavy"] = fuzz.trimf(snow.universe, [101.6, 152.4, 200])  # beyond 6 inches
     snow["very heavy"] = fuzz.trimf(snow.universe, [152.4, 176.2, 200])
-
-    wind["gentle"] = fuzz.trimf(wind.universe, [0, 0, 24])
-    wind["moderate"] = fuzz.trimf(wind.universe, [19, 31, 44])
-    wind["strong"] = fuzz.trimf(wind.universe, [39, 44, 49])
 
     risk["low"] = fuzz.trimf(risk.universe, [0, 0, 4])
     risk["medium"] = fuzz.trimf(risk.universe, [3, 5, 7])
@@ -53,8 +53,8 @@ def get_fuzzy_output(
 
     rules.append(ctrl.Rule(snow["light"] & wind["gentle"], risk["low"]))
     rules.append(ctrl.Rule(snow["moderate"] | wind["moderate"], risk["medium"]))
-    rules.append(ctrl.Rule(snow["heavy"] & wind["moderate"], risk["high"]))
-    rules.append(ctrl.Rule(snow["very heavy"] | wind["strong"], risk["high"]))
+    rules.append(ctrl.Rule(snow["heavy"] | wind["strong"], risk["high"]))
+    rules.append(ctrl.Rule(snow["very heavy"], risk["high"]))
 
     # Create control system
     risk_ctrl = ctrl.ControlSystem(rules)
--- test_fuzzylogic.py
+++ test_fuzzylogic.py
@@ -1,11 +1,48 @@
 import pytest
 from fuzzylogic import get_fuzzy_output
 
+"""
+    wind["gentle"] =        [0, 0, 20]
+    wind["moderate"] =      [19, 31, 41]
+    wind["strong"] =        [39, 44, 49]
+
+    snow["light"] =         [0, 0, 76.2] # up to 3 inches
+    snow["moderate"] =      [50.8, 101.6, 152.4] # Starts at 2 inches, ends at 6 inches
+    snow["heavy"] =         [101.6, 152.4, 200]  # beyond 6 inches
+    snow["very heavy"] =    [152.4, 176.2, 200]
+"""
+
 
 def test_normal_case():
     # Normal case should not raise any exceptions and should return a float
     result = get_fuzzy_output(12.55, 23.4)
     assert isinstance(result, str), "The result should be a str"
+
+
+def test_light_snow_and_gentle_wind_return_low_risk():
+    result = get_fuzzy_output(12.55, 13.4)
+    assert result == "Low"
+
+
+def test_moderate_snow_or_moderate_wind_return_medium_risk():
+    result = get_fuzzy_output(10, 93.4)
+    assert result == "Medium"
+
+    result = get_fuzzy_output(24, 100)
+    assert result == "Medium"
+
+
+def test_heavy_snow_or_strong_wind_return_high_risk():
+    result = get_fuzzy_output(48, 10)
+    assert result == "High"
+
+    result = get_fuzzy_output(10, 150)
+    assert result == "High"
+
+
+def test_very_heavy_snow_return_high_risk():
+    result = get_fuzzy_output(10, 180)
+    assert result == "High"
 
 
 def test_inputs_are_int():
